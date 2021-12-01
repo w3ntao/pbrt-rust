8a1118804703e974801337e400cc323380df8c32
Wentao Liu <wentao.liu.zero@outlook.com>
2021-12-01 17:53:36 +0800

    render simple image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,48 @@
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
+mod vector;
+
+use crate::vector::*;
 
 fn main() {
-    println!("Hello, Rust!");
+    const IMAGE_WIDTH: usize = 256;
+    const IMAGE_HEIGHT: usize = 256;
+
+    let mut pixels = [[Color::zero(); IMAGE_WIDTH]; IMAGE_HEIGHT];
+    let factor = 256 as f32 - 0.001;
+    for h in (0usize..IMAGE_HEIGHT).rev() {
+        for w in 0usize..IMAGE_WIDTH {
+            let red = w as f32 / (IMAGE_WIDTH - 1) as f32;
+            let green = h as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let blue = 0.25;
+
+            pixels[h][w] = Vector::new(red * factor,
+                                       green * factor,
+                                       blue * factor);
+        }
+    }
+    let pixels = pixels;
+    //discard mut
+    {
+        let ppm_head = format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+        let ppm_file_name = "out.ppm";
+        fs::write(ppm_file_name, ppm_head)
+            .expect(&format!("Failed to write to `{}`", ppm_file_name));
+
+        let mut file = OpenOptions::new()
+            .append(true)
+            .open(ppm_file_name)
+            .unwrap();
+        for h in (0usize..IMAGE_HEIGHT).rev() {
+            for w in 0usize..IMAGE_WIDTH {
+                write!(file, "{} {} {}\n",
+                       pixels[h][w].x as i32,
+                       pixels[h][w].y as i32,
+                       pixels[h][w].z as i32)
+                    .expect(&format!("Failed to append to `{}`", ppm_file_name));
+            }
+        }
+    }
 }
---
+++ src/vector.rs
@@ -0,0 +1,127 @@
+use std::ops;
+
+#[derive(Default, Copy, Clone)]
+pub struct Vector {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vector {
+    pub fn new(_x: f32, _y: f32, _z: f32) -> Self {
+        return Self {
+            x: _x,
+            y: _y,
+            z: _z,
+        };
+    }
+    pub fn zero() -> Self {
+        return Default::default();
+    }
+
+    pub fn length_squared(self) -> f32 {
+        return dot(self, self);
+    }
+
+    pub fn length(self) -> f32 {
+        return self.length_squared().sqrt();
+    }
+}
+
+impl ops::Neg for Vector {
+    type Output = Vector;
+    fn neg(self) -> Vector {
+        return Vector {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        };
+    }
+}
+
+impl ops::Add<Vector> for Vector {
+    type Output = Vector;
+    fn add(self, rhs: Vector) -> Vector {
+        return Vector {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        };
+    }
+}
+
+impl ops::Sub<Vector> for Vector {
+    type Output = Vector;
+    fn sub(self, rhs: Vector) -> Vector {
+        return Vector {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        };
+    }
+}
+
+impl ops::Mul<Vector> for Vector {
+    type Output = Vector;
+    fn mul(self, rhs: Vector) -> Vector {
+        return Vector {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        };
+    }
+}
+
+impl ops::Mul<f32> for Vector {
+    type Output = Vector;
+    fn mul(self, rhs: f32) -> Vector {
+        return Vector {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        };
+    }
+}
+
+impl ops::Mul<Vector> for f32 {
+    type Output = Vector;
+    fn mul(self, rhs: Vector) -> Vector {
+        return rhs * self;
+    }
+}
+
+impl ops::Div<f32> for Vector {
+    type Output = Vector;
+    fn div(self, rhs: f32) -> Vector {
+        assert_ne!(rhs, 0.0);
+        return self * (1.0 / rhs);
+    }
+}
+
+impl ops::AddAssign<Vector> for Vector {
+    fn add_assign(&mut self, rhs: Vector) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl ops::MulAssign<Vector> for Vector {
+    fn mul_assign(&mut self, rhs: Vector) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
+    }
+}
+
+impl ops::DivAssign<f32> for Vector {
+    fn div_assign(&mut self, rhs: f32) {
+        *self = *self / rhs;
+    }
+}
+
+pub fn dot(a: Vector, b: Vector) -> f32 {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+pub type Color = Vector;
