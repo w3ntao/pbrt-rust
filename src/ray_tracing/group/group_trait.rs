708ef9dbaf41b4cacd2fce18759eb189622124ce
Wentao Liu <wentao.liu.zero@outlook.com>
2021-12-03 19:14:20 +0800

    feature: now group is also a primitive

--- src/ray_tracing/group/group_trait.rs
+++ src/ray_tracing/group/group_trait.rs
@@ -2,8 +2,13 @@ use crate::ray_tracing::ray::*;
 use crate::ray_tracing::intersection::*;
 use crate::ray_tracing::primitive_trait::Primitive;
 
-
 pub trait GroupTrait<'a> {
     fn add(&mut self, p: &'a dyn Primitive);
     fn intersect(&self, ray: &Ray, previous_distance: f32) -> Intersection;
+}
+
+impl<'a> Primitive for dyn GroupTrait<'a> {
+     fn intersect(&self, ray: &Ray, previous_distance: f32) -> Intersection {
+         return self.intersect(ray, previous_distance);
+     }
 }
