bd2bf786d7920e7d5cc4c229f4bf23ca11b38e97
Wentao Liu <wentao.liu.zero@outlook.com>
2022-04-15 08:02:52 +0800

    feature: add DiffuseLight to textures

---
+++ src/ray_tracing/textures/diffuse_light.rs
@@ -0,0 +1,30 @@
+use std::sync::Arc;
+
+use crate::fundamental::color::Color;
+use crate::fundamental::point::Point;
+use crate::ray_tracing::intersection::Intersection;
+use crate::ray_tracing::material::Material;
+use crate::ray_tracing::ray::Ray;
+use crate::ray_tracing::texture::Texture;
+
+pub struct DiffuseLight {
+    emission: Arc<dyn Texture>,
+}
+
+impl DiffuseLight {
+    pub fn new(_texture: Arc<dyn Texture>) -> DiffuseLight {
+        DiffuseLight {
+            emission: _texture,
+        }
+    }
+}
+
+impl Material for DiffuseLight {
+    fn scatter(&self, _: &Ray, _: &Intersection, _: &mut Ray) -> Color {
+        return Color::black();
+    }
+
+    fn emit(&self, u: f32, v: f32, point: Point) -> Color {
+        return self.emission.get_color(u, v, point);
+    }
+}
--- src/ray_tracing/textures/mod.rs
+++ src/ray_tracing/textures/mod.rs
@@ -2,3 +2,4 @@ mod perlin;
 pub mod solid_color;
 pub mod checker_texture;
 pub mod noise_texture;
+pub mod diffuse_light;
