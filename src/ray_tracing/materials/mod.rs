c40729ba01f4e71356592fe1ae34422baeeda613
Wentao Liu <wentao.liu.zero@outlook.com>
2022-06-15 07:32:37 +0800

    refactor: move diffuse_light.rs to materials

--- src/ray_tracing/materials/mod.rs
+++ src/ray_tracing/materials/mod.rs
@@ -2,3 +2,4 @@ pub mod lambertian;
 pub mod metal;
 pub mod mirror;
 pub mod glass;
+pub mod diffuse_light;
--- src/ray_tracing/textures/mod.rs
+++ src/ray_tracing/textures/mod.rs
@@ -2,4 +2,3 @@ mod perlin;
 pub mod solid_color;
 pub mod checker_texture;
 pub mod noise_texture;
-pub mod diffuse_light;
