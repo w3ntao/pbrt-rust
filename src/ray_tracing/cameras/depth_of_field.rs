aabad4ada07289b6e3e36cc239ca3931dca38f90
Wentao Liu <wentao.liu.zero@outlook.com>
2022-06-17 08:03:04 +0800

    refactor: implement (incomplete) stratified sampling for DoF

--- src/ray_tracing/cameras/depth_of_field.rs
+++ src/ray_tracing/cameras/depth_of_field.rs
@@ -1,3 +1,6 @@
+use rand::{Rng, thread_rng};
+use rand::distributions::Uniform;
+
 use crate::fundamental::point::*;
 use crate::fundamental::vector3::*;
 use crate::ray_tracing::camera::Camera;
@@ -59,6 +62,45 @@ impl Camera for DepthOfField {
     }
 
     fn get_stratified_rays(&self, num_samples: u32, min_u: f32, max_u: f32, min_v: f32, max_v: f32) -> Vec<Ray> {
-        panic!("get_stratified_ray() is not implemented for DepthOfField");
+        // TODO: stratified aperture sampling
+        let mut generator_u = thread_rng();
+        let mut generator_v = thread_rng();
+
+        let samples_per_dimension = (num_samples as f32).sqrt() as u32;
+
+        let mut rays = vec![];
+
+        let u_unit = (max_u - min_u) / (samples_per_dimension as f32);
+        let v_unit = (max_v - min_v) / (samples_per_dimension as f32);
+
+        for u_idx in 0..samples_per_dimension {
+            let u_idx = u_idx as f32;
+            let range_u =
+                Uniform::new(min_u + u_unit * u_idx, min_u + u_unit * (u_idx + 1.0));
+
+            for v_idx in 0..samples_per_dimension {
+                let v_idx = v_idx as f32;
+                let range_v =
+                    Uniform::new(min_v + v_unit * v_idx, min_v + v_unit * (v_idx + 1.0));
+
+                let u = generator_u.sample(range_u);
+                let v = generator_v.sample(range_v);
+
+                let x = u * self.x_pixel_multiplier;
+                let y = v * self.y_pixel_multiplier;
+                let direction = self.forward + x * self.horizontal + y * self.vertical;
+
+                let target = self.center + direction.normalize() * self.focus_distance;
+
+                let (rd_x, rd_y) = random_vector_in_disk();
+                let rd_x = rd_x * self.aperture / 2.0;
+                let rd_y = rd_y * self.aperture / 2.0;
+                let origin = self.center + self.horizontal * rd_x + self.vertical * rd_y;
+
+                rays.push(Ray::new(origin, (target - origin).normalize()));
+            }
+        }
+
+        return rays;
     }
 }
