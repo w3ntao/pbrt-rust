e27ba50ee0f78b4aba42cfeaafafcd6f618d3871
Wentao Liu <wentao.liu.zero@outlook.com>
2022-07-21 10:08:49 +0800

    deploy GitHub actions

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod fundamental;
 mod ray_tracing;
 mod test_cases;
 
-fn main() {
-    let mut samples = 20;
+fn all_tests(samples: u32) {
     println!();
 
     let mut width = 1000;
@@ -22,15 +21,18 @@ fn main() {
     test_case_10_perlin_texture::test(width, height, samples);
     test_case_11_lighting::test(width, height, samples);
 
-    samples = 50;
-    width = 600;
-    height = 600;
+    width = 500;
+    height = 500;
 
     test_case_12_cornell_box_monte_carlo::test(width, height, samples);
     test_case_13_cornell_box_next_event_estimation::test(width, height, samples);
     test_case_14_cornell_box_specular_monte_carlo::test(width, height, samples);
     test_case_15_cornell_box_specular_next_event_estimation::test(width, height, samples);
 
-    test_case_16_cornell_box_metal_dragon_next_event_estimation::test(width, height, samples);
+    test_case_16_cornell_box_metal_dragon_next_event_estimation::test(width, height, 10);
     test_case_17_smallpt::test(1024, 762, samples);
+}
+
+fn main() {
+    all_tests(10);
 }
