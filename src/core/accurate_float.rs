2465599970355e4762d660430543ae7c40a8c7c7
Wentao Liu <wentao.liu.zero@outlook.com>
2022-10-27 15:35:44 +0800

    feature: implement accurate float computation

--- src/core/accurate_float.rs
+++ src/core/accurate_float.rs
@@ -8,3 +8,12 @@ pub fn next_float_up(x: f32) -> f32 {
     let bits = x.to_bits();
     return f32::from_bits(if x >= 0.0 { bits + 1 } else { bits - 1 });
 }
+
+pub fn next_float_down(x: f32) -> f32 {
+    if x.is_infinite() || x.is_zero() {
+        return x;
+    }
+
+    let bits = x.to_bits();
+    return f32::from_bits(if x >= 0.0 { bits - 1 } else { bits + 1 });
+}
