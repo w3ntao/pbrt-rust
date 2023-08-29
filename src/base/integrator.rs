10a3c2dd15c6b3909b75cd4151abba6cee6e3575
Wentao Liu <wentao.liu.zero@outlook.com>
2023-08-29 11:12:41 +0800

    refactor: decopule Li() from evaluate_pixel_sample()

--- src/base/integrator.rs
+++ src/base/integrator.rs
@@ -5,9 +5,28 @@ pub trait Integrator: Send + Sync {
         &self,
         p_pixel: Point2i,
         num_samples: usize,
-        aggregate: Arc<dyn Primitive>,
+        aggregate: Arc<BVHAggregate>,
         sampler: &mut dyn Sampler,
         camera: Arc<dyn Camera>,
         film: &mut Arc<Mutex<dyn Film>>,
-    );
+    ) {
+        // TODO: rewrite sampler initialization
+        // TODO: rewrite this function to evaluate a row in a time
+        // to reduce concurrent access to shared data
+
+        let filter = film.lock().unwrap().get_filter().clone();
+        let mut accumulated_spectrum = RGBColor::black();
+        for _ in 0..num_samples {
+            let camera_sample = sampler.get_camera_sample(p_pixel.clone(), filter.clone());
+            let camera_ray = camera.generate_camera_ray(camera_sample);
+
+            accumulated_spectrum += self.Li(camera_ray, aggregate.clone());
+        }
+
+        film.lock()
+            .unwrap()
+            .add_sample(p_pixel, accumulated_spectrum / (num_samples as Float));
+    }
+
+    fn Li(&self, camera_ray: Ray, aggregate: Arc<BVHAggregate>) -> RGBColor;
 }
--- src/integrators/surface_normal_visualizer.rs
+++ src/integrators/surface_normal_visualizer.rs
@@ -9,38 +9,12 @@ impl SurfaceNormalVisualizer {
 }
 
 impl Integrator for SurfaceNormalVisualizer {
-    fn evaluate_pixel_sample(
-        &self,
-        p_pixel: Point2i,
-        num_samples: usize,
-        aggregate: Arc<dyn Primitive>,
-        sampler: &mut dyn Sampler,
-        camera: Arc<dyn Camera>,
-        film: &mut Arc<Mutex<dyn Film>>,
-    ) {
-        // TODO: rewrite sampler initialization
-        // TODO: rewrite this function to evaluate a row in a time
-        // to reduce concurrent access to shared data
-
-        let filter = film.lock().unwrap().get_filter().clone();
-        let mut accumulated_spectrum = RGBColor::black();
-        for _ in 0..num_samples {
-            let camera_sample = sampler.get_camera_sample(p_pixel.clone(), filter.clone());
-
-            let camera_ray = camera.generate_camera_ray(camera_sample);
-
-            match aggregate.intersect(&camera_ray, Float::INFINITY) {
-                None => {}
-                Some(shape_intersection) => {
-                    accumulated_spectrum += Vector3f::from(shape_intersection.normal)
-                        .normalize()
-                        .softmax_color();
-                }
-            };
-        }
-
-        film.lock()
-            .unwrap()
-            .add_sample(p_pixel, accumulated_spectrum / (num_samples as Float));
+    fn Li(&self, camera_ray: Ray, aggregate: Arc<BVHAggregate>) -> RGBColor {
+        return match aggregate.intersect(&camera_ray, Float::INFINITY) {
+            None => RGBColor::black(),
+            Some(shape_intersection) => Vector3f::from(shape_intersection.normal)
+                .normalize()
+                .softmax_color(),
+        };
     }
 }
