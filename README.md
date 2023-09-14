087d690eff430665049826f0009b7c6318c7fec1
Wentao Liu <wentao.liu.zero@outlook.com>
2023-09-14 21:45:32 +0800

    docs: add README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+## build and render
+
+clone pbrt-minus and scenes repo:
+```
+$ git clone https://github.com/w3ntao/pbrt-minus.git
+$ git clone https://github.com/w3ntao/pbrt-minus-scenes.git
+```
+
+build and render test scenes:
+```
+$ cd pbrt-minus
+$ cargo build --release
+$ bash render_all.sh
+```
--- render_all.sh
+++ render_all.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# you should have pbrt-minus-scene (https://github.com/w3ntao/pbrt-minus-scene)
-# cloned in the same level as pbrt-minus before running this script
-
 red_start="\033[31m"
 red_end="\033[0m"
 
-for scene_file in ../pbrt-minus-scene/*/*.json; do
-    printf "rendering: ${red_start}${scene_file}${red_end}\n"
-    ./target/release/pbrt-minus --spp=1 ${scene_file}
+scene_dir="../pbrt-minus-scenes"
+if [ ! -d "${scene_dir}" ]; then
+  echo "please download scenes from ${red_start}https://github.com/w3ntao/pbrt-minus-scenes${red_end}"
+  echo "to the same level as pbrt-minus"
+  exit 1
+fi
+
+mode="$1"
+if [ "$mode" = "--debug" ]; then
+    mode="debug"
+else
+    mode="release"
+fi;
+
+for scene_file in ${scene_dir}/*/*.json; do
+    command="./target/${mode}/pbrt-minus --spp=1"
+    printf "${command} ${red_start}${scene_file}${red_end}\n"
+    eval "${command} ${scene_file}"
     printf " \n"
 done
