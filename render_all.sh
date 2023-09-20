5eebfc0d495e7f8286933b9bc49dad62ac65308a
Wentao Liu <wentao.liu.zero@outlook.com>
2023-09-20 23:28:26 +0800

    CI: rewrite GitHub actions

--- render_all.sh
+++ render_all.sh
@@ -5,9 +5,9 @@ red_end="\033[0m"
 
 scene_dir="../pbrt-minus-scenes"
 if [ ! -d "${scene_dir}" ]; then
-  echo "please download scenes from ${red_start}https://github.com/w3ntao/pbrt-minus-scenes${red_end}"
-  echo "to the same level as pbrt-minus"
-  exit 1
+    echo "please download scenes from ${red_start}https://github.com/w3ntao/pbrt-minus-scenes${red_end}"
+    echo "to the same level as pbrt-minus"
+    exit 1
 fi
 
 mode="$1"
@@ -15,11 +15,14 @@ if [ "$mode" = "--debug" ]; then
     mode="debug"
 else
     mode="release"
-fi;
+fi
 
 for scene_file in ${scene_dir}/*/*.json; do
     command="./target/${mode}/pbrt-minus --spp=1"
     printf "${command} ${red_start}${scene_file}${red_end}\n"
-    eval "${command} ${scene_file}"
+    eval "${command} ${scene_file}" || {
+        echo 'fail'
+        exit 1
+    }
     printf " \n"
 done
